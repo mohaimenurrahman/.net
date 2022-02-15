646d1cbc472538b62c4525ef46dd348a9038cb4a
mohaimenurrahman <mohaimenur888@gmail.com>
2022-02-15 16:48:06 +0600

    Try

---
+++ 1st_task/1st_task/Controllers/CVController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1st_task.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}

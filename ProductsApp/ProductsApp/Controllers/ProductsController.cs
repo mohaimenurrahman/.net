a105cba2f1794cdf224ddc191f034faa26dd854c
mohaimenurrahman <mohaimenur888@gmail.com>
2022-02-15 16:50:53 +0600

    product

---
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductsApp.Models.Database;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+
+        public ActionResult Index()
+        {
+            DBEntities db = new DBEntities();
+            var data = db.productsdetails.Tolist();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductsApp.Models.Database.productsdetails n)
+        {
+            if (ModelState.IsValid)
+            {
+                DBEntities db = new DBEntities();
+                db.ProductsApp.Add(n);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
